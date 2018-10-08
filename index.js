b3eeaeb0535c226a2a2f2562370aa2b2f5ee584b
KULAK Vincent <vincent.kulak@gima.fr>
2018-10-08 15:19:47 +0200

    Add update feature

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = function(sails) {
     var setup = require('./lib/onkyo.setup.js');
     var init = require('./lib/onkyo.init.js');
     var exec = require('./lib/onkyo.exec.js');
+    var update = require('./lib/onkyo.update.js');
 
 
     gladys.on('ready', function() {
@@ -12,6 +13,7 @@ module.exports = function(sails) {
     return {
         setup,
         init,
-        exec
+        exec,
+        update
     };
 };
