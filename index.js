6ceb6b1511e005ba49ebdf4a199ccf459ab364c6
KULAK Vincent <vincent.kulak@gima.fr>
2018-10-12 16:52:52 +0200

    🎨 Lint

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 module.exports = function(sails) {
 
-    var setup = require('./lib/onkyo.setup.js');
-    var init = require('./lib/onkyo.init.js');
-    var exec = require('./lib/onkyo.exec.js');
-    var update = require('./lib/onkyo.update.js');
+  var setup = require('./lib/onkyo.setup.js');
+  var init = require('./lib/onkyo.init.js');
+  var exec = require('./lib/onkyo.exec.js');
+  var update = require('./lib/onkyo.update.js');
 
+  gladys.on('ready', function() {
+    init();
+  });
 
-    gladys.on('ready', function() {
-        init();
-    });
-
-    return {
-        setup,
-        init,
-        exec,
-        update
-    };
+  return {
+    setup,
+    init,
+    exec,
+    update
+  };
 };
